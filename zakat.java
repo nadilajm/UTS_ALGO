f9dd227d0efe4d85fd672b17deb3f2ea0894d3fa
Nadila JM <96558056+nadilajm@users.noreply.github.com>
2023-04-09 09:43:33 +0700

    Add files via upload

---
+++ zakat.java
@@ -0,0 +1,17 @@
+public class zakat {
+    public String nama;
+    public String alamat;
+    public String jenisZakat;
+    public int jmlhberas;
+    public double jumlhuang;
+
+    public zakat(String nama, String alamat, String jenisZakat, int jmlhberas, double jumlhuang){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.jenisZakat = jenisZakat;
+        this.jmlhberas = jmlhberas;
+        this.jumlhuang = jumlhuang;
+
+    }
+
+}
